f2c6bcfb154ced4af2467bac09dd80d61f8f00c0
johnsjohns <o-anao@hotmail.com>
2021-01-14 22:39:00 -0300

    Removido erro ao iniciar

--- PainelCombo.java
+++ PainelCombo.java
@@ -8,16 +8,18 @@ public class PainelCombo extends JPanel {
 
     public PainelCombo() {
         JASerial portas = new JASerial();
-        try{
-        seriais = new JComboBox(portas.getPort());
-        } catch (Exception e){
-            e.printStackTrace();
-        };
-        setLayout(new GridLayout(1,2));
-        JLabel lblSerial = new JLabel("Porta Serial:");
-        add(lblSerial);
-        add(seriais);
-        seriais.setSelectedIndex(0);
+        if(portas.getPort().length != 0){
+            try{
+                seriais = new JComboBox(portas.getPort());
+            } catch (Exception e){
+                e.printStackTrace();
+            };
+            setLayout(new GridLayout(1,2));
+            JLabel lblSerial = new JLabel("Porta Serial:");
+            add(lblSerial);
+            add(seriais);
+            seriais.setSelectedIndex(0);
+        }
     }
 
     public int getPort(){
